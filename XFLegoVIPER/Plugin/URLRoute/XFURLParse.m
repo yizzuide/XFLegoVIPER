4195cf25e570520d17aff07800f1058e4e749ed4
yizzuide <fu893695651@gmail.com>
2017-04-03 17:01:47 +0800

    support url value for component URLParams

--- XFLegoVIPER/Plugin/URLRoute/XFURLParse.m
+++ XFLegoVIPER/Plugin/URLRoute/XFURLParse.m
@@ -40,6 +40,9 @@
         if (element.count != 2) continue;
         NSString *key = element[0], *value = element[1];
         if (key.length == 0) continue;
+        if ([value hasPrefix:@"http%3A%2F%2F"]) {
+            value = [self decodeFromPercentEscapeString:value];
+        }
         queryDict[key] = value;
     }
     return [NSDictionary dictionaryWithDictionary:queryDict];
@@ -64,9 +67,39 @@
 + (NSString *)stringFromDictionary:(NSDictionary *)dict {
     NSMutableString *mStr = @"".mutableCopy;
     for (NSString *key in  dict.allKeys){
-        [mStr appendFormat:@"%@=%@&",key, dict[key]];
+        // 判断值是否为Http协议的URL
+        NSString *value = dict[key];
+        if ([value hasPrefix:@"http://"]) {
+            value = [self encodeToPercentEscapeString:value];
+            NSString *orignURL = [self decodeFromPercentEscapeString:value];
+            NSLog(@"%@",orignURL);
+        }
+        [mStr appendFormat:@"%@=%@&",key, value];
     }
     NSString *result = [mStr substringToIndex:mStr.length - 1];
     return result;
+}
+
++ (NSString *)encodeToPercentEscapeString:(NSString *)input
+{
+    NSString *outputStr =
+    (__bridge NSString *)CFURLCreateStringByAddingPercentEscapes(
+                                                                 NULL, /* allocator */
+                                                                 (__bridge CFStringRef)input,
+                                                                 NULL, /* charactersToLeaveUnescaped */
+                                                                 (CFStringRef)@"!*'();:@&=+$,/?%#[]",
+                                                                 kCFStringEncodingUTF8);
+    return outputStr;
+    
+}
+
++ (NSString *)decodeFromPercentEscapeString:(NSString *)input
+{
+    NSMutableString *outputStr = [NSMutableString stringWithString:input];
+    [outputStr replaceOccurrencesOfString:@"+"
+                               withString:@""
+                                  options:NSLiteralSearch
+                                    range:NSMakeRange(0,[outputStr length])];
+    return [outputStr stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
 }
 @end
